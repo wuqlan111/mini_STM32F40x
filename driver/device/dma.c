eadd238c7a63cef6e729eeea34c58902323757c0
wuqlan111 <wuqlan111@163.com>
2023-05-27 10:17:51 +0800

    Modify  dma.

--- driver/device/dma.c
+++ driver/device/dma.c
@@ -3,29 +3,65 @@
 #include  "../include/dma.h"
 
 
-typedef struct  dma_interrupt{
+#define  DMA_LISR_TCIF0             (1<<5)              // stream 0 transfer complete interrupt flag
+#define  DMA_LISR_HTIF0             (1<<4)              // stream 0 half transfer interrupt flag
+#define  DMA_LISR_TEIF0             0x8                 // stream 0 transfer error interrupt flag
+#define  DMA_LISR_DMEIF0            0x4                 // stream 0 direct mode error interrupt flag
+#define  DMA_LISR_FEIF0             0x1                 // stream 0 FIFO error interrupt flag 
 
-    volatile  uint32_t  dma_lisr;
-    volatile  uint32_t  dma_hisr;
-    volatile  uint32_t  dma_lifcr;
-    volatile  uint32_t  dma_hifcr;
+#define  DMA_LIFCR_CTCIF0           (1<<5)                     // stream 0 clear transfer complete interrupt flag 
+#define  DMA_LIFCR_CHTIF0           (1<<4)                     // stream 0 clear half transfer interrupt flag
+#define  DMA_LIFCR_CTEIF0           0x8                        // stream x clear transfer error interrupt flag
+#define  DMA_LIFCR_CDMEIF0          0x4                      // stream x clear direct mode error interrupt flag
+#define  DMA_LIFCR_CFEIF0           0x1                     //  stream x clear FIFO error interrupt flag
+#define  DMA_ALLC_INT               0x3d
 
-}dma_interrupt_t;
+#define  DMA_SxCR_CHSEL         (0x7<<25)                   // Channel selection
+#define  DMA_SxCR_MBURST        (0x3<<23)                   // memory burst transfer configuration
+#define  DMA_SxCR_PBURST        (0x3<<21)                   // peripheral burst transfer configuration
+#define  DMA_SxCR_CT            (1<<19)                     // current target memory is memory 1
+#define  DMA_SxCR_DBM           (1<<18)                     // double buffer mode
+#define  DMA_SxCR_PL            (0x3<<16)                   // priority level
+#define  DMA_SxCR_PINCOS            (1<<15)                 // peripheral increment offset size
+#define  DMA_SxCR_MSIZE             (0x3<<13)                   // memory data size
+#define  DMA_SxCR_PSIZE             (0x3<<11)                   // peripheral data size
+#define  DMA_SxCR_MINC              (1<<10)                     // memory increment mode
+#define  DMA_SxCR_PINC              (1<<9)                      // peripheral increment mode
+#define  DMA_SxCR_CIRC              (1<<8)                      // circular mode enable
+#define  DMA_SxCR_DIR               (0x3<<6)                    // data transfer direction
+#define  DMA_SxCR_PFCTRL            (1<<5)                      // the peripheral is the flow controller
+#define  DMA_SxCR_TCIE              (1<<4)                         // transfer complete interrupt enable
+#define  DMA_SxCR_HTIE              0x8                         // half transfer interrupt enable
+#define  DMA_SxCR_TEIE              0x4                         // transfer error interrupt enable
+#define  DMA_SxCR_DMEIE             0x2                         // direct mode error interrupt enable
+#define  DMA_SxCR_EN                0x1                         // stream enabled
+#define  DMA_ALL_INTERRUPTE        0x8e
+
+#define  DMA_SxNDTR_NDT            0xffff                   // number of data items to transfer
+#define  DMA_SxPAR_PAR          0xffffffff              // peripheral address
+#define  DMA_SxM0AR_M0A         0xffffffff              // memory 0 address
+#define  DMA_SxM1AR_M1A         0xffffffff              // memory 1 address
 
 
-typedef struct  dma_stream{
+#define  DMA_SxFCR_FEIE             (1<<7)              // FIFO error interrupt enable
+#define  DMA_SxFCR_FS               0x38                // FIFO status
+#define  DMA_SxFCR_DMDIS            0x4                 // direct mode disable
+#define  DMA_SxFCR_FTH              0x3                 // FIFO threshold selection
 
-    volatile  uint32_t  dma_sxcr;
-    volatile  uint32_t  dma_sxndtr;
-    volatile  uint32_t  dma_sxpar;
-    volatile  uint32_t  dma_sxm0ar;
-    volatile  uint32_t  dma_sxm1ar;
-    volatile  uint32_t  dma_sxfcr;
+#define  DMA_REGS_STEP               (0x400u)
+#define  DMA_REG_BASE_ADDR           (0x40026000u)
 
-}dma_stream_t;
-
-dma_interrupt_t * const dma_interrupt_control;
-dma_stream_t * const dma_all_stream[DMA_STREAM_NUMBER];
+#define  DMA_LISR_REG_ADDR(dma)                (DMA_REG_BASE_ADDR + (dma) * DMA_REGS_STEP )
+#define  DMA_HISR_REG_ADDR(dma)                (DMA_REG_BASE_ADDR + (dma) * DMA_REGS_STEP + 0x4u)
+#define  DMA_LIFCR_REG_ADDR(dma)               (DMA_REG_BASE_ADDR + (dma) * DMA_REGS_STEP + 0x8u)
+#define  DMA_HIFCR_REG_ADDR(dma)               (DMA_REG_BASE_ADDR + (dma) * DMA_REGS_STEP + 0xCu)
+#define  DMA_STREAM_REGS_BASE_ADDR(dma)        (DMA_REG_BASE_ADDR + (dma) * DMA_REGS_STEP + 0x10u)
+#define  DMA_SXCR_REG_ADDR(dma, stream)        (DMA_STREAM_REGS_BASE_ADDR(dma) + 0x18 * (steam))
+#define  DMA_SXNDTR_REG_ADDR(dma, stream)      (DMA_STREAM_REGS_BASE_ADDR(dma) + 0x18 * (steam) + 0x4u)
+#define  DMA_SXPAR_REG_ADDR(dma, stream)       (DMA_STREAM_REGS_BASE_ADDR(dma) + 0x18 * (steam) + 0x8u)
+#define  DMA_SXM0AR_REG_ADDR(dma, stream)      (DMA_STREAM_REGS_BASE_ADDR(dma) + 0x18 * (steam) + 0xCu)
+#define  DMA_SXM1AR_REG_ADDR(dma, stream)      (DMA_STREAM_REGS_BASE_ADDR(dma) + 0x18 * (steam) + 0x10u)
+#define  DMA_SXFCR_REG_ADDR(dma, stream)       (DMA_STREAM_REGS_BASE_ADDR(dma) + 0x18 * (steam) + 0x14u)
 
 
 int32_t  get_stream_interrupt(uint8_t stream_idx){
--- driver/include/dma.h
+++ driver/include/dma.h
@@ -2,64 +2,62 @@
 #ifndef  STM32_DMA_H
 #define  STM32_DMA_H
 
-
 #include  <stdint.h>
-
-
-#define  DMA_LISR_TCIF0             (1<<5)              // stream 0 transfer complete interrupt flag
-#define  DMA_LISR_HTIF0             (1<<4)              // stream 0 half transfer interrupt flag
-#define  DMA_LISR_TEIF0             0x8                 // stream 0 transfer error interrupt flag
-#define  DMA_LISR_DMEIF0            0x4                 // stream 0 direct mode error interrupt flag
-#define  DMA_LISR_FEIF0             0x1                 // stream 0 FIFO error interrupt flag 
-
-
-#define  DMA_LIFCR_CTCIF0           (1<<5)                     // stream 0 clear transfer complete interrupt flag 
-#define  DMA_LIFCR_CHTIF0           (1<<4)                     // stream 0 clear half transfer interrupt flag
-#define  DMA_LIFCR_CTEIF0           0x8                        // stream x clear transfer error interrupt flag
-#define  DMA_LIFCR_CDMEIF0          0x4                      // stream x clear direct mode error interrupt flag
-#define  DMA_LIFCR_CFEIF0           0x1                     //  stream x clear FIFO error interrupt flag
-#define  DMA_ALLC_INT               0x3d
-
-
-
-#define  DMA_SxCR_CHSEL         (0x7<<25)                   // Channel selection
-#define  DMA_SxCR_MBURST        (0x3<<23)                   // memory burst transfer configuration
-#define  DMA_SxCR_PBURST        (0x3<<21)                   // peripheral burst transfer configuration
-#define  DMA_SxCR_CT            (1<<19)                     // current target memory is memory 1
-#define  DMA_SxCR_DBM           (1<<18)                     // double buffer mode
-#define  DMA_SxCR_PL            (0x3<<16)                   // priority level
-#define  DMA_SxCR_PINCOS            (1<<15)                 // peripheral increment offset size
-#define  DMA_SxCR_MSIZE             (0x3<<13)                   // memory data size
-#define  DMA_SxCR_PSIZE             (0x3<<11)                   // peripheral data size
-#define  DMA_SxCR_MINC              (1<<10)                     // memory increment mode
-#define  DMA_SxCR_PINC              (1<<9)                      // peripheral increment mode
-#define  DMA_SxCR_CIRC              (1<<8)                      // circular mode enable
-#define  DMA_SxCR_DIR               (0x3<<6)                    // data transfer direction
-#define  DMA_SxCR_PFCTRL            (1<<5)                      // the peripheral is the flow controller
-#define  DMA_SxCR_TCIE              (1<<4)                         // transfer complete interrupt enable
-#define  DMA_SxCR_HTIE              0x8                         // half transfer interrupt enable
-#define  DMA_SxCR_TEIE              0x4                         // transfer error interrupt enable
-#define  DMA_SxCR_DMEIE             0x2                         // direct mode error interrupt enable
-#define  DMA_SxCR_EN                0x1                         // stream enabled
-#define  DMA_ALL_INTERRUPTE        0x8e
-
-
-#define  DMA_SxNDTR_NDT            0xffff                   // number of data items to transfer
-#define  DMA_SxPAR_PAR          0xffffffff              // peripheral address
-#define  DMA_SxM0AR_M0A         0xffffffff              // memory 0 address
-#define  DMA_SxM1AR_M1A         0xffffffff              // memory 1 address
-
-
-
-#define  DMA_SxFCR_FEIE             (1<<7)              // FIFO error interrupt enable
-#define  DMA_SxFCR_FS               0x38                // FIFO status
-#define  DMA_SxFCR_DMDIS            0x4                 // direct mode disable
-#define  DMA_SxFCR_FTH              0x3                 // FIFO threshold selection
-
+#include  "util.h"
 
 #define  DMA_STREAM_NUMBER      8
-#define  MAX_DMA_STREAM_ID      (DMA_STREAM_NUMBER - 1)
+#define  DMA_MAX_CHANNEL_NUMBER       8
 
+
+enum {
+    DMA1 = 0,
+    DMA2,
+    DMA_MAX_ID  =  DMA2,
+};
+
+
+enum {
+    DMA_SINGLE_TRANSFER = 0,
+    DMA_INCR4_TRANSFER,
+    DMA_INCR8_TRANSFER,
+    DMA_INCR16_TRANSFER,
+    DMA_MAX_TRANSFER  =  DMA_INCR16_TRANSFER,
+};
+
+enum {
+    DMA_LOW_PRIORITY = 0,
+    DMA_Medium_PRIORITY,
+    DMA_High_PRIORITY,
+    DMA_VERY_HIGH_PRIORITY,
+    DMA_MAX_PRIORITY  =  DMA_VERY_HIGH_PRIORITY,
+};
+
+enum {
+    DMA_DATA_SIZE_BYTE = 0,
+    DMA_DATA_SIZE_HALF_WORD,
+    DMA_DATA_SIZE_WORD,
+    DMA_DATA_MAX_SIZE = DMA_DATA_SIZE_WORD,
+};
+
+enum {
+    DMA_PERIPHERAL_TO_MEMORY  = 0,
+    DMA_MEMORY_TO_PERIPHERAL,
+    DMA_MEMORY_TO_MEMORY,
+    DMA_TRANSFER_MAX_DIRECTION = DMA_MEMORY_TO_MEMORY,
+};
+
+
+typedef struct {
+    uint32_t  channel:3;
+    uint32_t  memory_burst:2;
+    uint32_t  peripheral_burst:2;
+    uint32_t  memory0:1;
+    uint32_t  double_buffer_mode:1;
+    uint32_t  priority:2;
+    
+
+
+}DMA_stream_config_t;
 
 
 
