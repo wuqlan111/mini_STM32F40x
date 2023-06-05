32e5e24545a5def7fa8707cf745d5b53751340d5
wuqlan111 <wuqlan111@163.com>
2023-06-05 23:33:41 +0800

    Modify  crc.c

---
+++ driver/device/crc.c
@@ -0,0 +1,29 @@
+
+#include <stdlib.h>
+
+#include  "crc.h"
+
+
+
+
+
+#define  CRC_REGS_BASE_ADDR                        (0x40023000u)
+#define  CRC_DR_REG_ADDR                           (CRC_REGS_BASE_ADDR)
+#define  CRC_IDR_REG_ADDR                          (CRC_REGS_BASE_ADDR + 0x4)
+#define  CRC_CR_REG_ADDR                           (CRC_REGS_BASE_ADDR + 0x8)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ driver/include/crc.h
@@ -0,0 +1,21 @@
+
+#ifndef  STM32_CRC_H
+#define  STM32_CRC_H
+
+#include  <stdint.h>
+#include  "util.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
