52705da8ad2f9a82d29e620e615824ff7f905330
wuqlan111 <wuqlan111@163.com>
2023-07-19 23:13:39 +0800

    Add rcc.

---
+++ driver/device/rcc.c
@@ -0,0 +1,21 @@
+
+#include  <stdlib.h>
+#include  <memory.h>
+
+#include  "../include/rcc.h"
+#include  "util.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ driver/include/rcc.h
@@ -0,0 +1,30 @@
+#ifndef  STM32_RCC_H
+#define  STM32_RCC_H
+
+#include  <stdint.h>
+
+#include  "util.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
+
