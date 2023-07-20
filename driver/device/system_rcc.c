f0e99658296cb5349b708d672b5d42e87bb78957
wuqlan111 <wuqlan111@163.com>
2023-07-20 23:19:48 +0800

    Modify  system_rcc.

--- driver/device/system_rcc.c
+++ driver/device/system_rcc.c
@@ -3,11 +3,25 @@
 #include  <memory.h>
 
 #include  "../include/system_rcc.h"
+#include  "../include/driver_util.h"
 #include  "util.h"
 
 
 
+int32_t  rcc_module_set_op(rcc_module_e  module,  rcc_module_op_e  op)
+{
 
+    CHECK_PARAM_VALUE(module,  RCC_MODULE_MAX);
+    CHECK_PARAM_VALUE(op,      RCC_MODULE_MAX_OP);
+
+
+    
+
+
+
+
+    return  0;
+}
 
 
 
