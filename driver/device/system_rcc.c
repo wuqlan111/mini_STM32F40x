2c3a03bbb69577943d9e60e8607856c7b8eab697
wuqlan111 <wuqlan111@163.com>
2023-07-21 00:29:35 +0800

    Modify  system_rcc.

--- driver/device/system_rcc.c
+++ driver/device/system_rcc.c
@@ -27,29 +27,89 @@
 
 static  int32_t  set_ahb1_module_op(rcc_module_e  module,  rcc_module_op_e  op)
 {
-    uint32_t  flag,   mask;
-    flag  =  mask  =  0;
-    if (module > RCC_MODULE_AHB1_MAX ) {
-        return   -1;
+    uint32_t  flag,   mask, shift;
+    flag  =  mask   =  shift  =  0;
+
+    CHECK_PARAM_VALUE(module, RCC_MODULE_AHB1_MAX );
+    CHECK_PARAM_VALUE(op,   RCC_MODULE_MAX_OP);
+
+    switch (op) {
+        case  RCC_RESET_OP:
+            flag  =   1;
+        case  RCC_UNRESET_OP:
+            mask  =   1;
+
+            if (module  < RCC_MODULE_CRC) {
+                shift  =  module - RCC_MODULE_GPIOA;
+            } else if (module < RCC_MODULE_DMA1) {
+                shift  =  module - RCC_MODULE_CRC + 12;
+            } else if (module < RCC_MODULE_ETHMAC) {
+                shift  =  module - RCC_MODULE_DMA1 + 21;
+            } else if (module < RCC_MODULE_OTGFS) {
+                shift  =  module - RCC_MODULE_ETHMAC + 25;
+            } else {
+                shift  =  module - RCC_MODULE_OTGHS + 29;
+            }
+
+            flag  <<= shift;
+            mask  <<= shift;
+            
+            break;
+
+        case  RCC_CLK_ENABLE:
+        case  RCC_CLK_ENABLE_LOWER_POWER:
+            if (module == RCC_MODULE_ETHMAC) {
+                flag  =  0xf;
+            } else if (module ==  RCC_MODULE_OTGHS) {
+                flag  =  0x3;
+            } else {
+                flag  =  1;
+            }
+
+        case  RCC_CLK_DISABLE:
+        case  RCC_CLK_DISABLE_LOWER_POWER:
+
+            if (module == RCC_MODULE_ETHMAC) {
+                mask  =  0xf;
+            } else if (module ==  RCC_MODULE_OTGHS) {
+                mask  =  0x3;
+            } else {
+                mask  =  1;
+            }
+
+            if (module  < RCC_MODULE_CRC) {
+                shift  =  module - RCC_MODULE_GPIOA;
+            } else if (module < RCC_MODULE_DMA1) {
+                shift  =  module - RCC_MODULE_CRC + 12;
+            } else if (module < RCC_MODULE_ETHMAC) {
+                shift  =  module - RCC_MODULE_DMA1 + 21;
+            } else if (module < RCC_MODULE_OTGFS) {
+                shift  =  module - RCC_MODULE_ETHMAC + 25;
+            } else {
+                shift  =  module - RCC_MODULE_OTGHS + 29;
+            }
+            flag  <<= shift;
+            mask  <<= shift;
     }
 
-    switch (op)
-    {
 
-        
-
+    switch (op) {
+        case  RCC_RESET_OP:
+        case  RCC_UNRESET_OP:
+            REG32_UPDATE(RCC_AHBXRSTR_REG_ADDR(RCC_AHB1_CLK), flag,  mask);
+            break;
 
         case  RCC_CLK_ENABLE:
         case  RCC_CLK_DISABLE:
-            if ()
+            REG32_UPDATE(RCC_AHBXENR_REG_ADDR(RCC_AHB1_CLK), flag,  mask);
             break;
-    
-    default:
-        break;
+        
+        default:
+            REG32_UPDATE(RCC_AHBXLPENR_REG_ADDR(RCC_AHB1_CLK), flag,  mask);
+            
     }
 
-
-
+    return   0;
 
 }
 
