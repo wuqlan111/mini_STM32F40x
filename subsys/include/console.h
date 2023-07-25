d6afb6f4ee33039eb82c6a11b44a849264c1066b
wuqlan111 <wuqlan111@163.com>
2023-07-25 23:44:31 +0800

    Modify console.

--- subsys/include/console.h
+++ subsys/include/console.h
@@ -29,17 +29,17 @@ void   __debug_printf(uint32_t level, char * fmt, ...);
 void  set_system_log_level(uint32_t level);
 
 
-#define  DEBUG_PRINTF_DEBUG(fmt, ...)     __debug_printf(LOG_LEVEL_DEBUG, "[DEBUG]%s:%u - " fmt "\r\n",  \
+#define  DEBUG_PRINTF_DEBUG(fmt, ...)     __debug_printf(LOG_LEVEL_DEBUG, "<DEBUG>[%s:%u] - " fmt "\r\n",  \
                                                 __FILE__,  __LINE__, ##__VA_ARGS__)
-#define  DEBUG_PRINTF_INFO(fmt, ...)      __debug_printf(LOG_LEVEL_INFO, "[INFO]%s:%u - " fmt "\r\n",   \
+#define  DEBUG_PRINTF_INFO(fmt, ...)      __debug_printf(LOG_LEVEL_INFO, "<INFO>[%s:%u] - " fmt "\r\n",   \
                                                 __FILE__, __LINE__, ##__VA_ARGS__)
-#define  DEBUG_PRINTF_NOTICE(fmt, ...)    __debug_printf(LOG_LEVEL_NOTICE, "[NOTICE]%s:%u - " fmt "\r\n",   \
+#define  DEBUG_PRINTF_NOTICE(fmt, ...)    __debug_printf(LOG_LEVEL_NOTICE, "<NOTICE>[%s:%u] - " fmt "\r\n",   \
                                                 __FILE__, __LINE__, ##__VA_ARGS__)
-#define  DEBUG_PRINTF_WARN(fmt, ...)      __debug_printf(LOG_LEVEL_WARN, "[WARN]%s:%u - " fmt "\r\n",   \
+#define  DEBUG_PRINTF_WARN(fmt, ...)      __debug_printf(LOG_LEVEL_WARN, "<WARN>[%s:%u] - " fmt "\r\n",   \
                                                 __FILE__, __LINE__, ##__VA_ARGS__)
-#define  DEBUG_PRINTF_ERROR(fmt, ...)      __debug_printf(LOG_LEVEL_ERROR, "[ERROR]%s:%u - " fmt "\r\n",  \
+#define  DEBUG_PRINTF_ERROR(fmt, ...)      __debug_printf(LOG_LEVEL_ERROR, "<ERROR>[%s:%u] - " fmt "\r\n",  \
                                                 __FILE__, __LINE__, ##__VA_ARGS__)
-#define  DEBUG_PRINTF_FATAL(fmt, ...)      __debug_printf(LOG_LEVEL_FATAL, "[FATAL]%s:%u - " fmt "\r\n",  \
+#define  DEBUG_PRINTF_FATAL(fmt, ...)      __debug_printf(LOG_LEVEL_FATAL, "<FATAL>[%s:%u] - " fmt "\r\n",  \
                                                 __FILE__, __LINE__, ##__VA_ARGS__)
 
 
