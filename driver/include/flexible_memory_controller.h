37ff93af85b5b04ca3bf753191db975b0e0faf7d
wuqlan111 <wuqlan111@163.com>
2023-07-28 22:42:25 +0800

    Modify flexible_memory_controller.

--- driver/include/flexible_memory_controller.h
+++ driver/include/flexible_memory_controller.h
@@ -9,18 +9,38 @@
 
 
 
+typedef enum {
+    FSMC_CRAM_PAGE_128  =  1,
+    FSMC_CRAM_PAGE_256,
+    FSMC_CRAM_PAGE_512,
+    FSMC_CRAM_PAGE_1024,
+    FSMC_CRAM_MAX_PAGE  =  FSMC_CRAM_PAGE_1024,
+} fsmc_cram_page_size_e;
+
+
+typedef enum {
+    FSMC_MEMORY_WIDTH_8  =  0,
+    FSMC_MEMORY_WIDTH_16,
+    FSMC_MEMORY_MAX_WIDTH  =  FSMC_MEMORY_WIDTH_16,
+} fsmc_memory_width_e;
 
 
 
+typedef enum {
+    FSMC_MEMORY_SRAM  =  0,
+    FSMC_MEMORY_PSRAM,
+    FSMC_MEMORY_NOR_FLASH,
+    FSMC_MEMORY_MAX_TYPE  =  FSMC_MEMORY_NOR_FLASH,
+} fsmc_memory_type_e;
 
 
-
-
-
-
-
-
-
+typedef enum {
+    FSMC_ACCESS_MODE_A  =  0,
+    FSMC_ACCESS_MODE_B,
+    FSMC_ACCESS_MODE_C,
+    FSMC_ACCESS_MODE_D,
+    FSMC_ACCESS_MAX_MODE  =  FSMC_ACCESS_MODE_D,
+} fsmc_access_mode_e;
 
 
 
