fb322a65cef9a3179f4666f96d61c7af3e95c5b6
wuqlan111 <wuqlan111@163.com>
2023-07-29 09:02:45 +0800

    Modify  flexible_memory_controller.

--- driver/include/flexible_memory_controller.h
+++ driver/include/flexible_memory_controller.h
@@ -43,6 +43,61 @@ typedef enum {
 } fsmc_access_mode_e;
 
 
+typedef  union {
+    struct {
+        uint32_t   write_burst:1;
+        uint32_t   cram_page_size:3;
+        uint32_t   async_wait:1;
+        uint32_t   ext_mode:1;
+        uint32_t   wait_enable:1;
+        uint32_t   write_enable:1;
+        uint32_t   write_burst:1;
+        uint32_t   wait_active_high:1;
+        uint32_t   burst_enable:1;
+        uint32_t   memory_width:2;
+        uint32_t   memory_type:2;
+        uint32_t   addr_multiplex:1;
+    } sram_cfg;
+
+    struct {
+        uint32_t   ecc_page_size:3;
+        uint32_t   ale_re_delay:4;
+        uint32_t   cle_re_delay:4;
+        uint32_t   ecc_enable:1;
+        uint32_t   pbus_width:2;
+        uint32_t   nand_flash:1;
+        uint32_t   wait_enable:1;
+    } nand_flash_cfg;
+} fsmc_bank_control_t;
+
+
+
+
+typedef  union {
+    struct  {
+        uint32_t  access_mode:2;
+        uint32_t  bus_turn_phase:4;
+        uint32_t  data_phase:8;
+        uint32_t  addr_hold_phase:4;
+        uint32_t  addr_setup_phase:4;
+    } sram_time;
+
+    struct  {
+        uint32_t  memory_hiz_time:2;
+        uint32_t  memory_hold_time:4;
+        uint32_t  memory_wait_time:8;
+        uint32_t  memory_setup_time:4;
+    } sram_time;
+} fsmc_transfer_time_t;
+
+
+
+
+
+
+
+
+
 
 
 
