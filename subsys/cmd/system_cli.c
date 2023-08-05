f95fc11301d36304199180b913e0f392440af3ee
wuqlan111 <wuqlan111@163.com>
2023-08-05 14:25:57 +0800

    Modify system_cli.

--- subsys/cmd/system_cli.c
+++ subsys/cmd/system_cli.c
@@ -1,4 +1,5 @@
 
+#include  "3rd/gm_cli.h"
 
 
 
@@ -13,6 +14,16 @@
 
 
 
+void  system_cli_module_init(void)
+{
+    GM_CLI_Init();
+    /* 注册输出驱动 */
+    // GM_CLI_RegOutCharCallBack();
+    /* 设置提示符 */
+    GM_CLI_SetCommandPrompt("[STM32-CLI] > ");
+    /* 启动CLI */
+    GM_CLI_Start();
+}
 
 
 
